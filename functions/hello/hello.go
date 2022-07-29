01ba860752c661a08eb629a350b0b351d9233f6e
NguyenVux <nguyenhoangvub4@gmail.com>
2022-07-29 22:57:47 +0700

    Test function

--- functions/hello/hello.go
+++ functions/hello/hello.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	mm_url := "https://chat.gameloft.org/hooks/bcbu761yb3bufkmianijxjjfze"
+	type MatterMostPayload struct {
+		Channel string
+		Text    string
+	}
+	var payload MatterMostPayload = MatterMostPayload{
+		Channel: "@vu.nguyenhoang",
+		Text:    "Your Computer booted up",
+	}
+
+	var byteArr, _ = json.Marshal(payload)
+	req, err := http.NewRequest("POST", mm_url, bytes.NewBuffer(byteArr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World!",
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module main
 
 go 1.18
 
-require github.com/aws/aws-lambda-go v1.34.0
+require (
+	github.com/aws/aws-lambda-go v1.34.0
+)
